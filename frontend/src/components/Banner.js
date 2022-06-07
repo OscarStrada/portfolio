2fb49e8d673dd5c21a5a02a878cb3aff29b68a02
Oscar Estrada <arturostrada2020@gmail.com>
2022-06-07 17:11:43 -0500

    added content

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Loader } from './Loader';
 
 export const Banner = () => {
+  const title = 'Oscar Estrada \n y construyo cosas\nen internet';
   return (
-    <div className="w-full h-screen px-custom">
-      <div className="w-1/2 pt-40">
-        <h1 className="text-[85px] font-bold leading-custom">
-          <span className="text-green-custom">></span>Donde la Ciencia y el Arte
-          hacen Match<span className="text-green-custom">_</span>
+    <div className="w-full min-h-screen px-custom ">
+      <Loader />
+      <div className="pt-40 space-y-5">
+        <p>> console.log('Hola mundo, me llamo') _</p>
+        <h1 className="text-[100px] font-bold leading-custom whitespace-pre-line">
+          {title}
+          <span>_</span>
         </h1>
       </div>
     </div>
